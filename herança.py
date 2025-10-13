7428223048c935cb6949c934eba6bf94d8cd7c04
isabelli1309 <isoca.drive@gmail.com>
2025-10-13 18:19:49 -0300

    Update na classe

--- herança.py
+++ herança.py
@@ -1,1 +1,8 @@
 #classe veiculos
+Classe veiculos:
+    def_init_(self, rodas, marca):
+        self.rodas = rodas
+        self.marca = marca
+
+        def ligar(self):
+            print("vrummmm")
