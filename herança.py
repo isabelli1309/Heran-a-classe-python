f64d376198e7c641b536701e8ebd526d9a20b2ad
isabelli1309 <isoca.drive@gmail.com>
2025-10-20 18:07:52 -0300

    update herança

--- herança.py
+++ herança.py
@@ -1,8 +1,13 @@
 #classe veiculos
-Classe veiculos:
-    def_init_(self, rodas, marca):
+class veiculos:
+    def __init__(self, rodas, marca):
         self.rodas = rodas
         self.marca = marca
 
         def ligar(self):
             print("vrummmm")
+        
+class Carro(veiculo):
+    def __init__(self, rodas, marca, teto_solar):
+      super().__init__(rodas, marca)
+      self.teto_solar = teto_solar
